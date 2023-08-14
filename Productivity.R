fd5bfbe756bb79b748281f08bf3fa020b5e4f5c1
YamiliCarmona <yamiliruiz98@gmail.com>
2023-08-14 12:25:16 -0700

    productivity

--- Productivity.R
+++ Productivity.R
@@ -4,11 +4,18 @@ library(dplyr)
 library(tidyverse)
 library(ramify)
 library(ggplot2)
+library(openxlsx)
 
 # load data -----------------
 
-fish_ltem <- readRDS("data/LTEM_historic_updated_27122022.RDS") |> 
-  filter(Region == "La Paz", Reef=="ESPIRITU_SANTO_ISLOTES_ESTE", Label == "PEC") |>  
+tabla <- read.xlsx("data/tabl_parametrs.xlsx")
+
+
+fish <- readRDS("data/LTEM_historic_updated_27122022.RDS") |> 
+  mutate(Family = ifelse(Genus == "Rypticus", "Grammistidae", Family)) |> 
+  filter(Label == "PEC",
+         Family %in% c ("Serranidae", "Lutjanidae", "Carangidae", "Scaridae", "Scombridae"),
+  ) |> 
   mutate(
     A_ord = as.numeric(A_ord),
     B_pen= as.numeric(B_pen),
@@ -28,115 +35,86 @@ fish_ltem <- readRDS("data/LTEM_historic_updated_27122022.RDS") |>
     TrophicLevelF = cut(as.numeric(TrophicLevel), 
                         breaks = c(2, 2.5, 3, 3.5, 4, 4.6), 
                         labels = c("2-2.5", "2.5-3", "3-3.5", "3.5-4", "4-4.5"), 
-                        right = FALSE)) 
+                        right = FALSE)) |> 
+  mutate(Species = recode(Species, "Rypticus courtenayi   " = "Rypticus courtenayi",
+                          "Carangoides orthogrammus" = "Ferdauia orthogrammus",
+                          "Epinephelus acanthistius" = "Hyporthodus acanthistius")) 
+# 
+# fish_ltem <- left_join(fish_ltem, tabl, by = c( "Family", "Species", "A_ord", "B_pen"))
+# 
+merged_data <- merge(fish, tabla, by = c("Family", "Species", "A_ord", "B_pen"), all.x = TRUE)
 
+
+# fish_ltem <- read.xlsx("data/fish_parametros.xlsx") |> # merged_data 
+fish_ltem <- merged_data |> 
+  filter(Region == "La Paz", Reef == "ESPIRITU_SANTO_ISLOTES_ESTE") |> 
+  mutate(Diet = factor(Diet,  
+                       levels = c("HerMac", "HerDet", "Omnivr", "Plktiv", "InvSes", "InvMob", "FisCep")),
+         Position = factor(Position,  
+                           levels = c("PelgAs", "PelgDw", "BtPlAs", "BtPlDw", "BnthAs", "BnthDw")),
+         Method = factor(Method,
+                         levels = c("LenFrq", "MarkRc", "Otolth", "Unknown", "OthRin", "ScalRi")))
+
+# # SpecCode, Diet, Position, Method
+# fish_ltem <- merge(fish_ltem, db[, c('Species', 'SpecCode', 'Diet', 'Position', 'Method')],
+#                    by = 'Species', all.x = TRUE, suffixes = c("", ".db"))
+# 
+# # Si hay columnas duplicadas después de la combinación, eliminar las duplicadas de .db
+# duplicated_columns <- names(fish_ltem)[duplicated(names(fish_ltem))]
+# fish_ltem <- fish_ltem[, !grepl("\\.db$", names(fish_ltem)) | !names(fish_ltem) %in% duplicated_columns]
+# 
+# # Si hay columnas duplicadas después de la combinación, renombrar las columnas en .db
+# for (col in setdiff(names(fish_ltem), names(db))) {
+#   if (grepl("\\.db$", col)) {
+#     new_colname <- gsub("\\.db$", "", col)
+#     names(fish_ltem)[names(fish_ltem) == col] <- new_colname
+#   }
+# }
+# 
+# # Tallas maximas
 # fish_ltem <- fish_ltem |> 
-#   mutate(
-#     SpecCode = case_when(
-#       Species == "Acanthurus xanthopterus" ~ 1261,
-#       Species == "Anisotremus interruptus" ~ 8253,
-#       Species == "Balistes polylepis" ~ 4276,
-#       Species == "Caranx caballus" ~ 1900,
-#       Species == "Caranx sexfasciatus" ~ 1917,
-#       Species == "Cephalopholis panamensis" ~ 8761,
-#       Species == "Decapterus macarellus" ~ 993,
-#       Species == "Epinephelus labriformis" ~ 8196,
-#       Species == "Haemulon sexfasciatum" ~ 13718,
-#       Species == "Holacanthus passer" ~ 8294,
-#       Species == "Lutjanus argentiventris" ~ 1408,
-#       Species == "Mycteroperca rosacea" ~ 9398,
-#       Species == "Scarus ghobban" ~ 5548,
-#       Species == "Scarus rubroviolaceus" ~ 5555,
-#       Species == "Stegastes flavilatus" ~ 12512,
-#       Species == "Stegastes rectifraenum" ~ 12516
-#     ),
-#     Diet = case_when(
-#       Species == "Acanthurus xanthopterus" ~ "HerDet",
-#       Species == "Anisotremus interruptus" ~ "InvMob",
-#       Species == "Balistes polylepis" ~ "InvMob",
-#       Species == "Caranx caballus" ~ "FisCep",
-#       Species == "Caranx sexfasciatus" ~ "InvMob",
-#       Species == "Cephalopholis panamensis" ~ "FisCep",
-#       Species == "Decapterus macarellus" ~ "Plktiv",
-#       Species == "Epinephelus labriformis" ~ "FisCep",
-#       Species == "Haemulon sexfasciatum" ~ "BtPlDw",
-#       Species == "Holacanthus passer" ~ "InvSes",
-#       Species == "Lutjanus argentiventris" ~ "InvMob",
-#       Species == "Mycteroperca rosacea" ~ "InvMob",
-#       Species == "Scarus ghobban" ~ "HerDet",
-#       Species == "Scarus rubroviolaceus" ~ "HerDet",
-#       Species == "Stegastes flavilatus" ~ "Omnivr",
-#       Species == "Stegastes rectifraenum" ~ "Omnivr"
-#     ),
-#     Position = case_when(
-#       Species == "Acanthurus xanthopterus" ~ "BtPlDw",
-#       Species == "Anisotremus interruptus" ~ "BtPlDw",
-#       Species == "Balistes polylepis" ~ "BtPlAs",
-#       Species == "Caranx caballus" ~ "PelgAs",
-#       Species == "Caranx sexfasciatus" ~ "PelgAs",
-#       Species == "Cephalopholis panamensis" ~ "BnthDw",
-#       Species == "Decapterus macarellus" ~ "PelgAs",
-#       Species == "Epinephelus labriformis" ~ "BnthDw",
-#       Species == "Haemulon sexfasciatum" ~ "BtPlDw",
-#       Species == "Holacanthus passer" ~ "BnthDw",
-#       Species == "Lutjanus argentiventris" ~ "BtPlDw",
-#       Species == "Mycteroperca rosacea" ~ "BnthDw",
-#       Species == "Scarus ghobban" ~ "BtPlDw",
-#       Species == "Scarus rubroviolaceus" ~ "BtPlDw",
-#       Species == "Stegastes flavilatus" ~ "BnthDw",
-#       Species == "Stegastes rectifraenum" ~ "BnthDw"
-#     )
-#   )
-
-# SpecCode, Diet, Position, Method
-fish_ltem <- merge(fish_ltem, db[, c('Species', 'SpecCode', 'Diet', 'Position', 'Method')],
-                   by = 'Species', all.x = TRUE, suffixes = c("", ".db"))
-
-# Si hay columnas duplicadas después de la combinación, eliminar las duplicadas de .db
-duplicated_columns <- names(fish_ltem)[duplicated(names(fish_ltem))]
-fish_ltem <- fish_ltem[, !grepl("\\.db$", names(fish_ltem)) | !names(fish_ltem) %in% duplicated_columns]
-
-# Si hay columnas duplicadas después de la combinación, renombrar las columnas en .db
-for (col in setdiff(names(fish_ltem), names(db))) {
-  if (grepl("\\.db$", col)) {
-    new_colname <- gsub("\\.db$", "", col)
-    names(fish_ltem)[names(fish_ltem) == col] <- new_colname
-  }
-}
-
-# Tallas maximas
-fish_ltem <- fish_ltem |> 
-  group_by(Species) |> 
-  mutate(MaxSizeTL = max(Size)) |> 
-  ungroup()
+#   group_by(Species) |> 
+#   mutate(MaxSizeTL = max(Size)) |> 
+#   ungroup()
 # 
 # Lmeas <- fish_ltem$Size # Vector de tallas iniciales
 # a <- fish_ltem$A_ord
 # b <- fish_ltem$B_pen
-t <- 365
 
-# sstmean 
-fish_ltem <- fish_ltem |> 
-  filter(!is.na(SpecCode)) |> 
-  mutate(sstmean = 25.5) |> 
-  select(Family, Species, SpecCode, Size, MaxSizeTL, Diet, Position, A_ord, B_pen, Longitude, Latitude, sstmean)
+t <- 1
 
 # Calcular la masa corporal individual -----------
 
 fish_ltem <- fish_ltem |> 
-  # rename(Lmeas = Size, a =  A_ord , b = B_pen ) |> 
-  mutate(Mti = A_ord * (Size ^ B_pen))
-  # mutate(Mti = a * (Lmeas ^ b))
+  # rename(a = A_ord, b = B_pen, lon = Longitude, lat = Latitude, Lmeas = Size) |> 
+  # mutate(Mti = A_ord * (Size ^ B_pen))
+# mutate(Mti = a * (Lmeas ^ b))
+  mutate(Biomass = (Quantity * A_ord* (Size^B_pen))/(Area * 100))
 
 # Biomasa total del conjunto de peces (Biomasa en pie):
 
 biomasa_total <- fish_ltem |> 
-  summarise(B_t = sum(Mti))
+  
+  summarise(B_t = sum(Biomass))
+
+head(db)
+str(fish_ltem)
+unique(db$Diet)
+
+# sstmean 
+fish_ltem <- fish_ltem |> 
+  mutate(sstmean = 25.5) |> 
+  select(Family, Species, SpecCode, MaxSizeTL, Diet, 
+         Position, A_ord, B_pen, LinfTL, K, O, Longitude, Latitude, sstmean, Method)
+
+fish_ltem$SpecCode <-as.integer(fish_ltem$SpecCode)
+
+
 
 # Predicts standardised growth parameter Kmax for reef fishes ----------------
 # predKmax(traits, dataset, fmod, params = NULL, niter, nrounds = 150, verbose = 0, print_every = 1000, return = c('pred', 'relimp', 'models'), lowq = 0.25, uppq = 0.75) 
 
-Kmaxpred <- predKmax(traits = fish_ltem, fmod = ~ MaxSizeTL + sstmean + Diet + Position, niter = 1000)
+Kmaxpred <- predKmax(traits = fish_ltem, fmod = ~ sstmean + MaxSizeTL + Diet + Position + Method, niter = 1000)
 
 # Acceder al elemento 'pred' de la lista Kmax
 Kmax_pred <- Kmaxpred$pred
@@ -182,7 +160,9 @@ stochastic <- applyMstoch(M, t = t)
 # applyMstoch(M, t) # Generar muestras aleatorias de sobrevivencia
 
 
-# Productividad = Crecimiento Somático Total - Pérdidas debido a la Mortalidad ------
+# Productividad = Biomasa + Crecimiento Somático Total - Pérdidas debido a la Mortalidad ------
+
+# standing_biomass <- sum(Biomass)
 
 # Calcular el Crecimiento Somático Total (suma de ganancias de peso)
 crecimiento_somatico_total <- sum(resultado_pesos)
@@ -190,8 +170,9 @@ crecimiento_somatico_total <- sum(resultado_pesos)
 # Calcular las Pérdidas debido a la Mortalidad (suma de pérdidas de peso por mortalidad)
 perdidas_mortalidad <- sum(perdida_per_capita)
 
-# Calcular la Productividad
-productividad <- crecimiento_somatico_total - perdidas_mortalidad
+
+# Productividad
+productividad <- (biomasa_total + crecimiento_somatico_total - perdidas_mortalidad)
 
 
 print(productividad)
